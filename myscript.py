7fc6eb63112ea0d3b9d5c03c0d803abc0d926c73
GabSawka <gabriel.sawka@polymtl.ca>
2023-10-31 17:39:24 -0400

    var for badhash

--- myscript.py
+++ myscript.py
@@ -1,8 +1,9 @@
 import os
+import subprocess
 
 badhash = "e6b41c365cf51aa324345c82fa39a7a81059d9c4"
+badhash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
 goodhash="bfdccab909c32635457d41eeb6e7fed322026170"
-
 os.system(f'git bisect start {badhash} {goodhash}')
 os.system('git bisect run python manage.py test')
 os.system('git bisect reset')
